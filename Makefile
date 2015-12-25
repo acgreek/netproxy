0fe0300f2d870e75708be83b905eddb5c3a2a185
A C <agcarat@gmail.com>
2015-12-24 22:38:56 -0500

    use boost logging

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
-CXXFLAGS = -ggdb3 -Wall  -std=c++11
+CXXFLAGS = -ggdb3 -Wall -DBOOST_LOG_DYN_LINK -std=c++11
 LDFLAGS = 
-LDLIBS=-lboost_program_options  -lboost_system -lpthread
+LDLIBS=-lboost_log -lboost_program_options  -lboost_system -lpthread
 
 nproxy_server: nproxy_server.o
 	 $(CXX) $(LDFLAGS) $(TARGET_ARCH) $^ $(LOADLIBES) $(LDLIBS) -o $@
--- nproxy_server.cpp
+++ nproxy_server.cpp
@@ -1,23 +1,17 @@
 #include <iostream>
+#include <sstream>
 #include <boost/asio.hpp>
 #include <boost/enable_shared_from_this.hpp>
 #include <boost/shared_ptr.hpp>
 #include <boost/bind.hpp>
 #include <boost/program_options.hpp>
-
-
-bool verbose= false;
+#include <boost/log/trivial.hpp>
 
 class Connection  {
     public:
         Connection(boost::asio::io_service &io_service) : socket_(io_service) {};
         boost::asio::ip::tcp::socket  & socket() {return socket_;}
-
-        void start() {
-            if (verbose) std::cout << "connected\n";
-        }
         boost::asio::streambuf& buffer() {return buffer_;};
-
     private:
         boost::asio::streambuf buffer_;
         boost::asio::ip::tcp::socket socket_;
@@ -26,6 +20,7 @@ class Proxy : public boost::enable_shared_from_this<Proxy>{
     public:
         Proxy (boost::asio::io_service &io_service) : from_(io_service),to_(io_service) {};
         void start(std::string &host, std::string &port) {
+             BOOST_LOG_TRIVIAL(info) << "received connection from " << from_.socket().remote_endpoint().address().to_string() << ":" << from_.socket().remote_endpoint().port();
             boost::asio::ip::tcp::resolver resolver (to_.socket().get_io_service());
             const boost::asio::ip::tcp::resolver::iterator endpoint_iterator = resolver.resolve({host.c_str(), port.c_str()});
             boost::asio::async_connect(to_.socket(), endpoint_iterator , boost::bind(&Proxy::handle_remote_connect, shared_from_this(), boost::asio::placeholders::error));
@@ -35,35 +30,39 @@ class Proxy : public boost::enable_shared_from_this<Proxy>{
         }
         void handle_remote_connect(const boost::system::error_code& error) {
             if (error) {
-                std::cout << "remote connection error  :" << error.message()<< "\n";
+                BOOST_LOG_TRIVIAL(info) << "remote connection error " << connDescription_;
                 cancelAll();
                 return;
             }
-            if (verbose) std::cout << "remote connection success\n";
+            std::stringstream ss;
+            ss << from_.socket().remote_endpoint().address().to_string() << ":" << from_.socket().remote_endpoint().port() << " to " << to_.socket().remote_endpoint().address().to_string() << ":" << to_.socket().remote_endpoint().port();
+            connDescription_ = ss.str();
+            BOOST_LOG_TRIVIAL(info) << "remote connection established from " << connDescription_ ;
             setupLocalRead();
             setupRemoteRead();
         }
 
         void setupLocalRead(){
-            if (verbose) std::cout << "setupLocalRead\n";
+            BOOST_LOG_TRIVIAL(trace) << "setup local read "  << connDescription_;
             boost::asio::streambuf::mutable_buffers_type mutableBuffer = from_.buffer().prepare(1024);
             from_.socket().async_read_some(mutableBuffer,boost::bind(&Proxy::handleLocalRead, shared_from_this(), boost::asio::placeholders::error ,boost::asio::placeholders::bytes_transferred));
         }
 
         void handleLocalRead(const boost::system::error_code& error, size_t bytes){
             if (error) {
-                std::cout << "read local error :" << error.message()<< "\n";
+                BOOST_LOG_TRIVIAL(info) << "local connection error " << connDescription_ << " :" << error.message();
                 cancelAll();
                 from_.socket().close();
                 return;
             }
+            BOOST_LOG_TRIVIAL(trace) << "read " << bytes << " from local connection "  << connDescription_;
             from_.buffer().commit(bytes);
             boost::asio::async_write(to_.socket(), from_.buffer(), boost::bind(&Proxy::handleLocalWrite, shared_from_this(), boost::asio::placeholders::error ,boost::asio::placeholders::bytes_transferred));
         }
 
         void handleLocalWrite(const boost::system::error_code& error, size_t bytes){
             if (error) {
-                std::cout << "write local error :" << error.message()<< "\n";
+                BOOST_LOG_TRIVIAL(info) << "write local error " <<  connDescription_ << " :" << error.message();
                 cancelAll();
                 to_.socket().close();
                 return;
@@ -72,26 +71,26 @@ class Proxy : public boost::enable_shared_from_this<Proxy>{
         }
 
         void setupRemoteRead(){
-            if (verbose) std::cout << "setupRemoteRead\n";
+            BOOST_LOG_TRIVIAL(debug) << "setup remote read " << connDescription_;
             boost::asio::streambuf::mutable_buffers_type mutableBuffer = to_.buffer().prepare(1024);
             to_.socket().async_read_some(mutableBuffer, boost::bind(&Proxy::handleRemoteRead, shared_from_this(), boost::asio::placeholders::error ,boost::asio::placeholders::bytes_transferred));
         }
 
         void handleRemoteRead(const boost::system::error_code& error, size_t bytes){
             if (error) {
-                std::cout << "read remote error :" << error.message()<< "\n";
+                BOOST_LOG_TRIVIAL(info) << "read remote error "  <<  connDescription_ << " :" << error.message();
                 cancelAll();
                 to_.socket().close();
                 return;
             }
-            if (verbose) std::cout << "read remote connection " << bytes << "\n";
+            BOOST_LOG_TRIVIAL(trace) << "read " << bytes << " from remote connection " <<connDescription_;
             to_.buffer().commit(bytes);
             boost::asio::async_write(from_.socket(), to_.buffer(), boost::bind(&Proxy::handleRemoteWrite, shared_from_this(), boost::asio::placeholders::error ,boost::asio::placeholders::bytes_transferred));
 
         }
         void handleRemoteWrite(const boost::system::error_code& error, size_t bytes) {
             if (error) {
-                std::cout << "write remote error :" << error.message()<< "\n";
+                BOOST_LOG_TRIVIAL(info) << "write remote error " <<  connDescription_ << " :" << error.message() ;
                 cancelAll();
                 from_.socket().close();
                 return;
@@ -108,14 +107,17 @@ class Proxy : public boost::enable_shared_from_this<Proxy>{
         }
         Connection  from_;
         Connection  to_;
+        std::string connDescription_;
 };
 
 class Server{
     public:
         Server(boost::asio::io_service &io_service, int port, std::string &remoteHost, std::string remotePort) :remoteHost_(remoteHost),remotePort_(remotePort), acceptor_(io_service,boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(), port) ) {
+
         }
         void start() {
             new_session_.reset(new Proxy(acceptor_.get_io_service()));
+            BOOST_LOG_TRIVIAL(trace) << "acceptor listening on " << acceptor_.local_endpoint().address().to_string() << ":" << acceptor_.local_endpoint().port();
             acceptor_.async_accept(new_session_->from_socket(), boost::bind(&Server::handle_accept, this, new_session_, boost::asio::placeholders::error));
         }
     private:
@@ -128,7 +130,7 @@ class Server{
                 acceptor_.async_accept(new_session->from_socket(), boost::bind(&Server::handle_accept, this, new_session, boost::asio::placeholders::error));
             }
             else {
-                std::cerr << "handle accept error " << error << std::endl;
+                BOOST_LOG_TRIVIAL(error) << "accept error " << error.message();
             }
         }
         std::string remoteHost_;
@@ -149,7 +151,6 @@ int main(int argc, char * argv[]) {
         ("remoteport,P", boost::program_options::value<std::string>(&remotePort)->default_value("9001"), "remote port to connect to")
         ("remotehost,H", boost::program_options::value<std::string>(&remoteHost)->default_value("127.0.0.1"), "remote host to connect to")
         ;
-
     boost::program_options::variables_map vm;
     boost::program_options::store(boost::program_options::parse_command_line(argc, argv, desc), vm);
     boost::program_options::notify(vm);
@@ -165,6 +166,5 @@ int main(int argc, char * argv[]) {
     } catch (std::exception & e) {
         std::cerr << "exception : " << e.what() << std::endl;
     }
-
     return 0;
 }
